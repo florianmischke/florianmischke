2eb2bce9307f11fcef57379bb7970e6b95b6b02a
Florian Mischke <code@florianmischke.de>
2024-09-05 23:15:26 +0200

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,8 +1,10 @@
 jQuery(function($) {
     function focustrigger() {
-        var id = location.hash.substring(1)
-        var el = $('#'+id)
-        el.trigger('focus')
+        if(location.hash && location.hash != '') {
+            var id = location.hash.substring(1)
+            var el = $('#'+id)
+            el.trigger('focus')
+        }
     }
 
     $( window ).on( 'hashchange', function( e ) {
